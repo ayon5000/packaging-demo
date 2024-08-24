53d0adab34ed7f6a458fd8463a2f11240a6c4da9
Ayon Banerjee <ayon5000@gmail.com>
2024-08-24 13:57:37 +0530

    add tests v2

--- tests/test_binary_operations.py
+++ tests/test_binary_operations.py
@@ -1,16 +1,17 @@
 from mypackage.math.binary_operations import add_numbers
 import pytest
 
+
 @pytest.mark.parametrize(
-        argnames = "first_num, second_num, result, is_passed",
-        argvalues =[
-            (1,2,3, True),
-            (4,6,10, True),
-            (21,20,41, True),
-            (50,50,100, True),
-            (50,50,101, False),
-        ]
+    argnames="first_num, second_num, result, is_passed",
+    argvalues=[
+        (1, 2, 3, True),
+        (4, 6, 10, True),
+        (21, 20, 41, True),
+        (50, 50, 100, True),
+        (50, 50, 101, False),
+    ],
 )
 def test__binary_operations(first_num: int, second_num: int, result: int, is_passed: bool):
-    test_result = add_numbers(first_num,second_num) == result
+    test_result = add_numbers(first_num, second_num) == result
     assert test_result == is_passed
--- tests/test_slow.py
+++ tests/test_slow.py
@@ -1,7 +1,8 @@
 from mypackage.slow import slow_add
 import pytest
 
+
 @pytest.mark.slow
 def test__slow_add__successful():
-    sum_ = slow_add(1,2)
+    sum_ = slow_add(1, 2)
     assert sum_ == 3
